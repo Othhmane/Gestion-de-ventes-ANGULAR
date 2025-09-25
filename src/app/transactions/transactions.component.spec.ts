39af3ca2fdc5283b6db537b1946615f8adea0406
Othhmane <othmane.haddouche.dz@viacesi.fr>
2025-09-25 20:03:33 +0200

    pret avec mauvaise archi

--- src/app/transactions/transactions.component.spec.ts
+++ src/app/transactions/transactions.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CalculComponent } from './calcul.component';
+import { CalculComponent } from './transactions.component';
 
 describe('CalculComponent', () => {
   let component: CalculComponent;
